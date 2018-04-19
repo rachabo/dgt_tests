f817c15acbb959f28aae12505d23dbe021f400d9
rachabo <38354293+rachabo@users.noreply.github.com>
2018-04-19 17:17:12 +0200

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -8,6 +8,7 @@ require 'scraperwiki/simple_html_dom.php';
 // // Read in a page
 //$html = scraperwiki::scrape("http://foo.com");
 $html = scraperwiki::scrape("http://revista.dgt.es/es/test/");
+$servidor="http://revista.dgt.es";
 //
 // // Find something on the page using css selectors
 $dom = new simple_html_dom();
@@ -15,12 +16,47 @@ $dom->load($html);
 //print_r($dom->find("table.list"));
 
 // Find  preguntas
-foreach($dom->find('h4[class=tit_not]') as $element)
+/*foreach($dom->find('h4[class=tit_not]') as $element)
        echo "<B>".$element->innertext . '</B><br>';
 
+*/
+
+foreach($dom->find('article.test') as $articulo)
+{
+	//Extraigo imagen de la pregunta
+	foreach($articulo->find('img') as $imagen)
+	echo "<B>".$imagen->src . '</B><br>';
+	
+
+	echo "<img src='".$servidor.$imagen->src."'><br>";
+
+	
+
+	foreach($articulo->find('section.content_test') as $section)
+	{
+	       //Texto de la pregunta
+	       foreach($section->find('h4[class=tit_not]') as $pregunta)
+	       {
+		    echo "<B>".$pregunta->innertext . '</B><br>';
+	       }
+	       //Todas las respuestas posibles
+	       foreach($section->find('li') as $respuesta)
+	       {
+		     echo $respuesta->innertext . '<br>';
+
+	       }
+	       //Opción correcta
+	       foreach($section->find('p span.opcion') as $correcta)
+	       {
+		     echo "Correcta:".$correcta->innertext . '<br>';
+
+	       }
+	}
+}
 // Find respuestas
-foreach($dom->find('section.content_test li') as $element)
+/*foreach($dom->find('section.content_test li') as $element)
       echo $element->innertext . '<br>';
+*/
       //echo $element->href . '<br>'; 
 //
 // // Write out to the sqlite database using scraperwiki library
